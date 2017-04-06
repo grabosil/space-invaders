d7d86a67d47cb6103cb9c6d7842728e80f95fe5e
Silvio Grabowski <grs@Silvios-MBP-2.fritz.box>
2017-04-06 22:13:54 +0200

    build structure

---
+++ spaceinvaders.py
@@ -0,0 +1,19 @@
+import cocos.sprite
+import cocos.collision_model as cm
+import cocos.euclid as eu
+
+class Actor(cocos.sprite.Sprite):
+   def __init__(self, image, x, y):
+       super(Actor, self).__init__(image)
+       self.position = eu.Vector2(x, y)
+       self.cshape = cm.AARectShape(self.position, self.width / 2, self.height / 2)
+
+   def move(self, offset):
+       self.position += offset
+       self.cshape.center += offset
+
+   def update(self, elapsed):
+       pass
+
+   def collide(self, other):
+       pass
